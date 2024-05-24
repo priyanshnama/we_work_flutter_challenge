0fc70e9d888ff9b52e3e8695f688ec623d2a0670
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2024-05-25 00:01:47 +0530

    refresh functionality added

--- lib/service/we_movies_repository.dart
+++ lib/service/we_movies_repository.dart
@@ -5,27 +5,23 @@ import 'package:we_work_flutter_challenge/data/movie.dart';
 class WeMoviesRepository {
   static final WeMoviesRepository _instance = WeMoviesRepository._internal();
 
-  factory WeMoviesRepository() {
-    return _instance;
-  }
+  factory WeMoviesRepository() => _instance;
 
   WeMoviesRepository._internal();
 
   static const String _apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTg3ZTY4MDMyODIwMTIzZmQ0Yzg0YjQzNDhjYjc3ZCIsInN1YiI6IjY2Mjg5NDExOTFmMGVhMDE0YjAwOWU1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6zIM73Giwg5M4wP6MX8KDCpee7IMnpnLTZUyMpETb08';
   static const String _baseUrl = 'https://api.themoviedb.org/3/movie';
   static const String _configurationUrl = 'https://api.themoviedb.org/3/configuration';
+  static const Map<String, String> _headers = {
+    'Authorization': 'Bearer $_apiKey',
+    'accept': 'application/json',
+  };
 
   String? _secureBaseUrl;
   List<String>? _posterSizes;
 
   Future<void> _fetchConfiguration() async {
-    final response = await http.get(
-      Uri.parse(_configurationUrl),
-      headers: {
-        'Authorization': 'Bearer $_apiKey',
-        'accept': 'application/json',
-      },
-    );
+    final response = await http.get(Uri.parse(_configurationUrl), headers: _headers);
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
@@ -36,41 +32,21 @@ class WeMoviesRepository {
     }
   }
 
-  Future<List<Movie>> getTopRatedMovies() async {
-    final response = await http.get(
-      Uri.parse('$_baseUrl/top_rated?language=en-US&page=1'),
-      headers: {
-        'Authorization': 'Bearer $_apiKey',
-        'accept': 'application/json',
-      },
-    );
+  Future<List<Movie>> _fetchMovies(String endpoint) async {
+    final response = await http.get(Uri.parse(endpoint), headers: _headers);
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       final List<dynamic> moviesJson = data['results'];
       return moviesJson.map((json) => Movie.fromJson(json)).toList();
     } else {
-      throw Exception('Failed to load top-rated movies');
+      throw Exception('Failed to load movies from $endpoint');
     }
   }
 
-  Future<List<Movie>> getNowPlayingMovies() async {
-    final response = await http.get(
-      Uri.parse('$_baseUrl/now_playing?language=en-US&page=1'),
-      headers: {
-        'Authorization': 'Bearer $_apiKey',
-        'accept': 'application/json',
-      },
-    );
+  Future<List<Movie>> getTopRatedMovies() => _fetchMovies('$_baseUrl/top_rated?language=en-US&page=1');
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final List<dynamic> moviesJson = data['results'];
-      return moviesJson.map((json) => Movie.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load now-playing movies');
-    }
-  }
+  Future<List<Movie>> getNowPlayingMovies() => _fetchMovies('$_baseUrl/now_playing?language=en-US&page=1');
 
   Future<String> getFullImageUrl(String imagePath, {String size = 'w500'}) async {
     if (_secureBaseUrl == null || _posterSizes == null) {
@@ -78,7 +54,7 @@ class WeMoviesRepository {
     }
 
     if (!_posterSizes!.contains(size)) {
-      throw Exception('Invalid image size');
+      throw Exception('Invalid image size: $size');
     }
 
     return '$_secureBaseUrl$size$imagePath';
--- lib/ui/we_movies.dart
+++ lib/ui/we_movies.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:we_work_flutter_challenge/bloc/we_movies/now_playing_movies_bloc.dart';
 import 'package:we_work_flutter_challenge/bloc/we_movies/top_rated_movies_bloc.dart';
+import 'package:we_work_flutter_challenge/bloc/we_movies/we_movies_events.dart';
 import 'package:we_work_flutter_challenge/bloc/we_movies/we_movies_states.dart';
 import 'package:we_work_flutter_challenge/service/we_movies_repository.dart';
 import 'package:we_work_flutter_challenge/ui/util/gradient_line.dart';
 import 'package:we_work_flutter_challenge/ui/util/now_playing_section.dart';
 import 'package:we_work_flutter_challenge/ui/util/top_rated_movie_card.dart';
-
-import '../bloc/we_movies/we_movies_events.dart';
 
 class WeMovies extends StatefulWidget {
   const WeMovies({super.key});
@@ -18,97 +17,113 @@ class WeMovies extends StatefulWidget {
 }
 
 class _WeMoviesState extends State<WeMovies> {
-  final TextEditingController _searchController = TextEditingController();
+  final _searchController = TextEditingController();
+  late final TopRatedMoviesBloc topRatedMoviesBloc;
+  late final NowPlayingMoviesBloc nowPlayingMoviesBloc;
+
+  @override
+  void initState() {
+    topRatedMoviesBloc = TopRatedMoviesBloc(context.read<WeMoviesRepository>());
+    nowPlayingMoviesBloc = NowPlayingMoviesBloc(context.read<WeMoviesRepository>());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => TopRatedMoviesBloc(WeMoviesRepository())
-            ..add(FetchTopRatedMoviesEvent()),
+          create: (context) =>
+              topRatedMoviesBloc..add(FetchTopRatedMoviesEvent()),
         ),
         BlocProvider(
-          create: (context) => NowPlayingMoviesBloc(WeMoviesRepository())
-            ..add(FetchNowPlayingMoviesEvent()),
+          create: (context) =>
+              nowPlayingMoviesBloc..add(FetchNowPlayingMoviesEvent()),
         ),
       ],
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-              children: [
-                // Search bar
-                TextField(
-                  controller: _searchController,
-                  decoration: const InputDecoration(
-                    hintText: 'Search Movies by name ...',
-                    prefixIcon: Icon(Icons.search),
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                      borderSide: BorderSide.none,
+          child: RefreshIndicator(
+            onRefresh: () async {
+              topRatedMoviesBloc.add(FetchTopRatedMoviesEvent());
+              nowPlayingMoviesBloc.add(FetchNowPlayingMoviesEvent());
+            },
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  // Search bar
+                  TextField(
+                    controller: _searchController,
+                    decoration: const InputDecoration(
+                      hintText: 'Search Movies by name ...',
+                      prefixIcon: Icon(Icons.search),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 0, horizontal: 20),
                     ),
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 0, horizontal: 20),
                   ),
-                ),
-                const SizedBox(height: 20),
+                  const SizedBox(height: 20),
 
-                // Now Playing section
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Text("NOW PLAYING"),
-                    GradientLine(Colors.black),
-                  ],
-                ),
-                BlocBuilder<NowPlayingMoviesBloc, NowPlayingMoviesState>(
-                  builder: (context, state) {
-                    if (state is NowPlayingMoviesLoading) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (state is NowPlayingMoviesLoaded) {
-                      final nowPlayingMovies = state.nowPlayingMovies;
-                      return NowPlayingSection(nowPlayingMovies: nowPlayingMovies);
-                    } else if (state is NowPlayingMoviesError) {
-                      return Center(child: Text(state.message));
-                    } else {
-                      return Container();
-                    }
-                  },
-                ),
-                const SizedBox(height: 20),
+                  // Now Playing section
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text("NOW PLAYING"),
+                      GradientLine(Colors.black),
+                    ],
+                  ),
+                  BlocBuilder<NowPlayingMoviesBloc, NowPlayingMoviesState>(
+                    builder: (context, state) {
+                      if (state is NowPlayingMoviesLoading) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (state is NowPlayingMoviesLoaded) {
+                        final nowPlayingMovies = state.nowPlayingMovies;
+                        return NowPlayingSection(
+                            nowPlayingMovies: nowPlayingMovies);
+                      } else if (state is NowPlayingMoviesError) {
+                        return Center(child: Text(state.message));
+                      } else {
+                        return Container();
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 20),
 
-                // Top Rated section
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Text("TOP RATED"),
-                    GradientLine(Colors.black),
-                  ],
-                ),
-                BlocBuilder<TopRatedMoviesBloc, TopRatedMoviesState>(
-                  builder: (context, state) {
-                    if (state is TopRatedMoviesLoading) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (state is TopRatedMoviesLoaded) {
-                      final topRatedMovies = state.topRatedMovies;
-                      return Column(
-                        children: topRatedMovies
-                            .map((movie) => TopRatedMovieCard(movie: movie))
-                            .toList(),
-                      );
-                    } else if (state is TopRatedMoviesError) {
-                      return Center(child: Text(state.message));
-                    } else {
-                      return Container();
-                    }
-                  },
-                ),
-              ],
+                  // Top Rated section
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text("TOP RATED"),
+                      GradientLine(Colors.black),
+                    ],
+                  ),
+                  BlocBuilder<TopRatedMoviesBloc, TopRatedMoviesState>(
+                    builder: (context, state) {
+                      if (state is TopRatedMoviesLoading) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (state is TopRatedMoviesLoaded) {
+                        final topRatedMovies = state.topRatedMovies;
+                        return Column(
+                          children: topRatedMovies
+                              .map((movie) => TopRatedMovieCard(movie: movie))
+                              .toList(),
+                        );
+                      } else if (state is TopRatedMoviesError) {
+                        return Center(child: Text(state.message));
+                      } else {
+                        return Container();
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
