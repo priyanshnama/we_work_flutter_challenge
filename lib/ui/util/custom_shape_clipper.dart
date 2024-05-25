80880b5579715f3e9a692994f35417be9fb5c8b1
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2024-05-26 01:36:15 +0530

    doc format

--- lib/ui/util/custom_shape_clipper.dart
+++ lib/ui/util/custom_shape_clipper.dart
@@ -5,33 +5,41 @@ class CustomShapeClipper extends CustomClipper<Path> {
   final double topBarOffset;
   final double bottomHeight;
 
-  CustomShapeClipper({required this.topHeight, required this.bottomHeight, this.topBarOffset = 0});
+  CustomShapeClipper(
+      {required this.topHeight,
+      required this.bottomHeight,
+      this.topBarOffset = 0});
 
   @override
   Path getClip(Size size) {
     Path path = Path();
-    path.moveTo(0, topHeight * 3/2);
+    path.moveTo(0, topHeight * 3 / 2);
 
-    path.quadraticBezierTo(0, topHeight, topHeight/2, topHeight);
-    path.lineTo((size.width - topHeight)/2 + topBarOffset, topHeight);
+    path.quadraticBezierTo(0, topHeight, topHeight / 2, topHeight);
+    path.lineTo((size.width - topHeight) / 2 + topBarOffset, topHeight);
 
-    path.quadraticBezierTo(size.width / 2 + topBarOffset, topHeight, size.width / 2 + topBarOffset, topHeight/2);
-    path.quadraticBezierTo(size.width / 2 + topBarOffset, 0, (size.width + topHeight)/2 + topBarOffset, 0);
+    path.quadraticBezierTo(size.width / 2 + topBarOffset, topHeight,
+        size.width / 2 + topBarOffset, topHeight / 2);
+    path.quadraticBezierTo(size.width / 2 + topBarOffset, 0,
+        (size.width + topHeight) / 2 + topBarOffset, 0);
 
-    path.lineTo(size.width - topHeight/2, 0);
-    path.quadraticBezierTo(size.width, 0, size.width, bottomHeight/2);
-    path.lineTo(size.width, size.height - bottomHeight*3/2);
+    path.lineTo(size.width - topHeight / 2, 0);
+    path.quadraticBezierTo(size.width, 0, size.width, bottomHeight / 2);
+    path.lineTo(size.width, size.height - bottomHeight * 3 / 2);
 
     path.quadraticBezierTo(size.width, size.height - bottomHeight,
-        size.width - bottomHeight/2, size.height - bottomHeight);
-    path.quadraticBezierTo(size.width - bottomHeight, size.height - bottomHeight,
-        size.width - bottomHeight, size.height - bottomHeight/2);
+        size.width - bottomHeight / 2, size.height - bottomHeight);
+    path.quadraticBezierTo(
+        size.width - bottomHeight,
+        size.height - bottomHeight,
+        size.width - bottomHeight,
+        size.height - bottomHeight / 2);
     path.quadraticBezierTo(size.width - bottomHeight, size.height,
-        size.width - (bottomHeight * 3/2), size.height);
+        size.width - (bottomHeight * 3 / 2), size.height);
 
-    path.lineTo(topHeight/2, size.height);
-    path.quadraticBezierTo(0, size.height, 0, size.height - topHeight/2);
-    path.lineTo(0, topHeight*3/2);
+    path.lineTo(topHeight / 2, size.height);
+    path.quadraticBezierTo(0, size.height, 0, size.height - topHeight / 2);
+    path.lineTo(0, topHeight * 3 / 2);
 
     path.close();
     return path;
